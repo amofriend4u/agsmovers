3d210509d3373af92d2baf877fae187b38aafcb8
amofriend4u <amofriend4u@gmail.com>
2023-06-14 12:20:27 +0530

    msg

---
+++ assest/js/script.js
@@ -0,0 +1,9 @@
+// international box start text effect
+var typed = new typed(".autotype",{
+
+    string :["Coding","Sleeping","Eating"],
+    typeSpeed :150, 
+    backSpeed :150,
+    loop :true
+
+})
